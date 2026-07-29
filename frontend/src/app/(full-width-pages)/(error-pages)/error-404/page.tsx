0dc060919de17b160691820431f9fe6cde7cf163
Shruti Patil <shrutiprasad0504@gmail.com>
2026-07-29 13:09:32 +0530

    metadata removed

--- frontend/src/app/(full-width-pages)/(error-pages)/error-404/page.tsx
+++ frontend/src/app/(full-width-pages)/(error-pages)/error-404/page.tsx
@@ -4,12 +4,6 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export const metadata: Metadata = {
-  title: "Next.js Error 404 | TailAdmin - Next.js Dashboard Template",
-  description:
-    "This is Next.js Error 404 page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
-};
-
 export default function Error404() {
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen p-6 overflow-hidden z-1">
--- frontend/src/app/(private)/contacts/page.tsx
+++ frontend/src/app/(private)/contacts/page.tsx
@@ -7,12 +7,7 @@ import { Contact } from "@/types/contact";
 import { Metadata } from "next";
 import React from "react";
 
-export const metadata: Metadata = {
-  title: "Next.js Basic Table | TailAdmin - Next.js Dashboard Template",
-  description:
-    "This is Next.js Basic Table  page for TailAdmin  Tailwind CSS Admin Dashboard Template",
-  // other metadata
-};
+
 
 export default function BasicTables() {
   return (
